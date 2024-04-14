5b7d4dfd6aae03e39faf628b295a7a2c623ac19c
victorcorreadasilva <victorcorreadasilva00@gmail.com>
2024-04-13 23:03:27 -0300

    Mesclado alterações victor e leonardo - Houve alguns conflitos, então algumas alterações se perderam, como de espaçamento de titulo, header e paragrafos e tamanho dos cads

--- script/script.js
+++ script/script.js
@@ -3,7 +3,9 @@ const arrowDown = document.querySelectorAll(".fa-arrow-down");
 console.log(arrowDown);
 
 function Conteudo(el) {
-  var display = document.getElementById(el).style.display;
-  if (display == "none") document.getElementById(el).style.display = "block";
-  else document.getElementById(el).style.display = "none";
+    var display = document.getElementById(el).style.display;
+    if (display == "none")
+        document.getElementById(el).style.display = 'block';
+    else
+        document.getElementById(el).style.display = 'none';
 }
