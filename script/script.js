a6fd01b0b3b6002856fee5a188abe47265221cae
Leonardo Rimes <rimesleo@gmail.com>
2024-04-17 14:50:24 -0300

    versao do leonardo com as setas funcionando

--- script/script.js
+++ script/script.js
@@ -1,11 +1,39 @@
-const arrowDown = document.querySelectorAll(".fa-arrow-down");
+let arrowElements = [];
 
-console.log(arrowDown);
-
-function Conteudo(el) {
-    var display = document.getElementById(el).style.display;
-    if (display == "none")
-        document.getElementById(el).style.display = 'block';
-    else
-        document.getElementById(el).style.display = 'none';
+async function populateArrowElementsArray() {
+    await new Promise(function(resolve) {
+        document.addEventListener("DOMContentLoaded", function() {
+            let elements = document.querySelectorAll(".fa-arrow-down, .fa-arrow-up");
+            arrowElements = Array.from(elements);
+            resolve();
+        });
+    });
 }
+
+async function toggleArrowClass() {
+    await populateArrowElementsArray();
+    document.querySelectorAll('h3').forEach(function(h3) {
+        h3.addEventListener('click', function() {
+            let arrowDown = h3.querySelector('.fa-arrow-down');
+            let arrowUp = h3.querySelector('.fa-arrow-up');
+            if (arrowDown && arrowUp) {
+                if (arrowDown.classList.contains('displayArrowNone')) {
+                    arrowDown.classList.remove("displayArrowNone");
+                    arrowUp.classList.add("displayArrowNone");
+                } else {
+                    arrowDown.classList.add("displayArrowNone");
+                    arrowUp.classList.remove("displayArrowNone");
+                }
+                
+                // Lógica para mostrar/ocultar o conteúdo
+                var el = h3.dataset.conteudo;
+                var contentElement = document.getElementById(el);
+                contentElement.style.display = (contentElement.style.display === 'none') ? 'block' : 'none';
+            }
+        });
+    });
+}
+
+
+    toggleArrowClass();
+
