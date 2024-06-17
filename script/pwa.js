6fe013f0147d519ed01c83355ad698ce6e0b46c6
Victor Corrêa da Silva <victorcorreadasilva00@gmail.com>
2024-06-17 20:49:36 -0300

    Implemenado tela de login e service-worker.js para salvar as paginas já visitadas em cache para quando o usuario for acessar sem internet.

--- script/pwa.js
+++ script/pwa.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+        navigator.serviceWorker.register('../script/service-worker.js').then(function (registration) {
             console.log('Service Worker registrado com sucesso:', registration);
         }, function (error) {
             console.log('Falha ao registrar o Service Worker:', error);
--- script/service-worker.js
+++ script/service-worker.js
@@ -1,12 +1,65 @@
-self.addEventListener('install', function(event) {
-    console.log('Service Worker instalado');
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
+self.addEventListener('install', function (event) {
+  console.log('Service Worker instalado');
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+  '../**/*',
+  '../html',
+  '../html/*.html',
+  '../styles/',
+  '../styles/*.css',
+  '../script/',
+  '../script/*.js',
+  '../img/',
+  '../img/**/',
+  '../img/**/*.png'
+];
+
+// Instalando o Service Worker e fazendo cache dos recursos necessários
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
       })
-    );
-  });
-  
+  );
+});
+
+// Buscando os recursos do cache quando offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Atualizando o Service Worker e limpando o cache antigo
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
