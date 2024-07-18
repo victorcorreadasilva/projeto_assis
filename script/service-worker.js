e398057bcad2a6066dbbe6c69acf41b4307b021c
Victor Corrêa da Silva <victorcorreadasilva00@gmail.com>
2024-07-18 18:50:41 -0300

    removido cache

--- script/service-worker.js
+++ script/service-worker.js
@@ -3,63 +3,20 @@ self.addEventListener('install', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
 
-const CACHE_NAME = 'pwa-cache-v1';
-const urlsToCache = [
-  '../**/*',
-  '../html',
-  '../html/*.html',
-  '../styles/',
-  '../styles/*.css',
-  '../script/',
-  '../script/*.js',
-  '../img/',
-  '../img/**/',
-  '../img/**/*.png'
-];
-
-// Instalando o Service Worker e fazendo cache dos recursos necessários
+// Instalando o Service Worker
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+  console.log('Service Worker instalado');
 });
 
-// Buscando os recursos do cache quando offline
+// Buscando os recursos diretamente
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+  event.respondWith(fetch(event.request));
 });
 
-// Atualizando o Service Worker e limpando o cache antigo
+// Atualizando o Service Worker
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+  console.log('Service Worker ativado');
 });
